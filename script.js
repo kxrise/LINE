733c92dcb33daa8edf09eb04a17a5e7600d20dcc
kxrise <belltree501@gmail.com>
2025-07-05 09:13:23 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 window.onload = function() {
     const folderPath = "images/"; // 画像フォルダのパス
-    const imageFile = "image.jpg"; // 表示する画像ファイル名
+    const imageFile = "2025summer.jpg"; // 表示する画像ファイル名
 
     const imgElement = document.getElementById("displayed-image");
     imgElement.src = folderPath + imageFile;
