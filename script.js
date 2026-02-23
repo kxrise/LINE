a31670655b8106952a960e7e8332fa529c9d5830
kxrise <belltree501@gmail.com>
2026-02-23 16:20:56 +0900

    Change displayed image to 'line1.jpg'

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 window.onload = function() {
     const folderPath = "images/"; // 画像フォルダのパス
-    const imageFile = "2025summer.jpg"; // 表示する画像ファイル名
+    const imageFile = "line1.jpg"; // 表示する画像ファイル名
 
     const imgElement = document.getElementById("displayed-image");
     imgElement.src = folderPath + imageFile;
 };
+
